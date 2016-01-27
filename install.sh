519067510785777bebdeb0b10065d03e4ee21c31
Paweł Jan Czochański <pawel@symmetra.pl>
2016-01-27 09:49:51 +0100

    śro, 27 sty 2016, 09:49:51 CET

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-
+echo "Removing old configs"
 rm -rf ~/.vim
 rm -rf ~/.vimrc
 rm -rf ~/.bashrc
+rm -rf ~/.gitconfig
 
+echo "Installing new config"
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.vim ~/.vim
 ln -s $PWD/.bashrc ~/.bashrc
+ln -s $PWD/.gitconfig ~/.gitconfig
+
+echo "Finished"
