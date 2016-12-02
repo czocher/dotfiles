bcdf04f877e05f9ae96f1f426aa7bf21f605af27
Paweł Czochański <czochanski@gmail.com>
2016-12-02 08:54:19 +0100

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install yubikey-personalization-gui gimp libreoffice git yakuake 
+dnf install yubikey-personalization-gui gimp libreoffice git yakuake rust vim vim-X11
 
 bash -c 'su -c "curl http://folkswithhats.org/fedy-installer -o fedy-installer && chmod +x fedy-installer && ./fedy-installer"'
