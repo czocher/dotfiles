2aba5febe4686c0b36935a11dd45a4e3be142dfc
Paweł Czochański <czochanski@gmail.com>
2018-04-06 12:33:41 +0200

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -22,6 +22,3 @@ sudo dnf -y install https://prerelease.keybase.io/keybase_amd64.rpm
 run_keybase
 
 curl https://www.folkswithhats.org/installer | sudo bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-chsh -s /usr/bin/zsh
