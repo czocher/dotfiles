31792a0e1413df6d1cbd617f5a7e429765f786cb
Paweł Czochański <czochanski@gmail.com>
2018-04-06 12:35:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ if [[ $(id -u) -eq 0 ]] ; then echo "Please run as non-root" ; exit 1 ; fi
 # Kill the whole script on CTRL+C
 trap "exit" INT
 
-bash generic.sh
-
 if [[ -f /etc/fedora-release ]]; then
   bash fedora.sh
 fi
+
+bash generic.sh
